68f8159e94d275d07a3b99a1146a1f51af970e24
Marat <yusupmarat2@gmail.com>
2024-12-21 18:18:09 +0400

    cast fixes and formatting

--- src/IO/Core/Hunter.cpp
+++ src/IO/Core/Hunter.cpp
@@ -2,11 +2,20 @@
 
 namespace sw::core {
 
+static constexpr auto kHunterStrengthAttackDistance = 1;
+
 void Hunter::attack(Unit &targetUnit) {
-    //uint32_t distance = std::max(std::abs(targetUnit.getX() - x_), std::abs(targetUnit.getY() - y_));
-        // if (distance <= range_) {
-        //     //targetUnit.setHp();
-        // }
+  // Компилятор встает на дыбы (придется кастить к int'у)
+
+  uint32_t distance =
+      std::max(std::abs(static_cast<int>(targetUnit.getX() - x_)),
+               std::abs(static_cast<int>(targetUnit.getY() - y_)));
+
+  if (distance <= range_ && distance > kHunterStrengthAttackDistance) {
+    targetUnit.setHP(targetUnit.getHP() - agility_);
+  } else {
+    targetUnit.setHP(targetUnit.getHP() - strength_);
+  }
 }
 
-}
+} // namespace sw::core
--- src/IO/Core/Swordsman.cpp
+++ src/IO/Core/Swordsman.cpp
@@ -1,12 +1,13 @@
 #include "Swordsman.hpp"
-#include <cmath>
-
+#include <cstdlib>
 namespace sw::core {
 
 void Swordsman::attack(Unit &targetUnit) {
-   //if (std::abs(targetUnit.getX() - x_) <= 1 && std::abs(targetUnit.getY() - y_) <= 1) {
-     targetUnit.setHP(targetUnit.getHP() - strength_);
-   //}
+  // Компилятор встает на дыбы (придется кастить к int'у)
+  if (std::abs(static_cast<int>(targetUnit.getX() - x_)) <= 1 &&
+      std::abs(static_cast<int>(targetUnit.getY() - y_)) <= 1) {
+    targetUnit.setHP(targetUnit.getHP() - strength_);
+  }
 }
 
-}   // namespace sw::core
+} // namespace sw::core
